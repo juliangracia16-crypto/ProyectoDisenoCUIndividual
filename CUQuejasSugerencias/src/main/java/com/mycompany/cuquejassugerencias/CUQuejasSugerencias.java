85385acf910c4fa518c62fc216d8f7e76e1aebed
juliangracia16-crypto <juliangracia16@outlook.com>
2026-05-18 20:20:39 -0700

    javadoc subsistema

--- CUQuejasSugerencias/src/main/java/com/mycompany/cuquejassugerencias/CUQuejasSugerencias.java
+++ CUQuejasSugerencias/src/main/java/com/mycompany/cuquejassugerencias/CUQuejasSugerencias.java
@@ -28,15 +28,15 @@ import java.util.List;
 
         
 /**
- *
+ * 
  * @author Julian
  */
 public class CUQuejasSugerencias implements ICUQuejasSugerencias{
-    private IHistorialIncidentesBO historialIncidentesBO;
-    private IHistorialAtencionesBO historialAtencionesBO;
-    private ICatalogosBO catalogosBO;
-    private ILoginBO login;
-    private IHistorialesReportesGeneralBO reportesGeneralesBO;
+    private final IHistorialIncidentesBO historialIncidentesBO;
+    private final IHistorialAtencionesBO historialAtencionesBO;
+    private final ICatalogosBO catalogosBO;
+    private final ILoginBO login;
+    private final IHistorialesReportesGeneralBO reportesGeneralesBO;
     private final String RUTA_LOGO_REPORTE_PDF = "/logo gym.jpg";
     private final String TITULO_REPORTE_PDF = "REPORTE DE REGISTROS DE QUEJAS Y SUGERENCIAS";
 
@@ -50,24 +50,49 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         this.reportesGeneralesBO = reportesGeneralesBO;
     }
     
+    /**
+     * Metodo para consultar todas las categorias 
+     * @return una lista con las categorias
+     * @throws NegocioException si oucrre un error al consultar
+     */
     @Override
     public List<CategoriaDTO> cargarCatalogoCategorias() throws NegocioException {
         List<CategoriaDTO> categoriasDTO = catalogosBO.consultarCatalogoCategorias();
         return categoriasDTO;
     }
-
+    
+    /**
+     * Metoco para consultar todos los estados que puede tener un reporte
+     * @return una lista con todos los estados que pueden tener los reportes
+     * @throws NegocioException si ocurre un error al consultar 
+     */
     @Override
     public List<EstadoReporteDTO> cargarCatalogoEstados() throws NegocioException {
         List<EstadoReporteDTO> estadosDTO = catalogosBO.consultarCatalogoEstados();
         return estadosDTO;
     }
 
+    /**
+     * Metodo para consultar un reporte de incidente por su folio
+     * @param folio del reporte que se consultara
+     * @return el reporte de incidente correspondiente a ese folio
+     * @throws NegocioException si ocurre un error al consultar o
+     * si no se encuentra algun reporte con ese folio
+     */
     @Override
     public ReporteIncidenteDTO consultarReporteIncidentePorFolio(String folio) throws NegocioException {
         ReporteIncidenteDTO reporteIncidente = historialIncidentesBO.consultarReporteIncidentePorFolio(folio);
         return reporteIncidente;
     }
     
+    /**
+     * Metodo para consultar todos los reportes de incidentes con filtros
+     * @param filtros por los que se consultara (Por: nombre cliente, categoria del reporte, estado del reporte, fecha desde, fecha hasta)
+     * @return una lista con los reportes incidentes que coincidan con los filtros o
+     * regresa todos si los filtros van vacios.
+     * @throws NegocioException si ocurre un error al consultar o 
+     * si los filtros son invalidos
+     */
     @Override
     public List<ReporteIncidenteDTO> consultarReportesIncidentes(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException {
         validarNombreFiltro(filtros.cliente());
@@ -76,6 +101,15 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         return reportesIncidentes;
     }
     
+    /**
+     * Metodo para consultar todos los reportes de incidentes generados por un cliente
+     * Tambien pueden filtrarse los reportes generados por un cliente
+     * @param filtros por los que se consultara (Por: estado del reporte, categoria del reporte, fecha desde, fecha hasta).
+     * @return una lista con los reportes del cliente que coincidan con los filtros 
+     * o regresa todos los reportes si los filtros van vacios.
+     * @throws NegocioException si ocurre un error al consultar
+     * o si los filtros son invalidos.
+     */
     @Override
     public List<ReporteIncidenteDTO> consultarReportesIncidentesPorCliente(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException {
         validarFechasFiltro(filtros.fechaDesde(), filtros.fechaHasta());
@@ -83,6 +117,14 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         return reportesIncidentes;
     }
 
+    /**
+     * Metodo para consultar todos los reportes de atenciones con filtros
+     * @param filtros por los que se consultara (Por: nombre de cliente, estado del reporte, categoria del reporte, fecha desde fecha hasta)
+     * @return una lista con los reportes que coincidan con los filtros o 
+     * todos los reportes si los filtros van vacios. 
+     * @throws NegocioException si ocurre un error al consultar
+     * o si los filtros son invalidos.
+     */
     @Override
     public List<ReporteAtencionDTO> consultarReportesAtenciones(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException {
         validarNombreFiltro(filtros.cliente());
@@ -91,6 +133,14 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         return reportesAtenciones;
     }
 
+    /**
+     * Metodo para generar un reporte de incidente 
+     * @param reporteIncidente objeto con los datos necesarios para generar un nuevo 
+     * reporte de incidente 
+     * @return el reporte de incidente generado correctamente
+     * @throws NegocioException si ocurre un error al generar el reporte o 
+     * si no cumple con las validaciones establecidas
+     */
     @Override
     public ReporteIncidenteGeneradoDTO generarReporteIncidente(NuevoReporteIncidenteDTO reporteIncidente) throws NegocioException {
         validarAsuntoReporteIncidente(reporteIncidente.asunto());
@@ -102,6 +152,15 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         return reporteIncidenteGenerado;
     }
 
+    /**
+     * Metodo para atender/resolver un reporte de incidente
+     * @param reporteAtencion objeto con los datos necesarios para resolver 
+     * el reporte de incidente
+     * @return el reporte atencion generado despues de resolver correctamente
+     * el reporte de incidente
+     * @throws NegocioException si ocurre un error al resolver el reporte
+     * o si no cumple con las validaciones establecidas
+     */
     @Override
     public ReporteAtencionGeneradoDTO atenderReporteIncidente(AtenderReporteDTO reporteAtencion) throws NegocioException {
         validarSolucionReporteAtencion(reporteAtencion.solucion());
@@ -111,30 +170,64 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         ReporteAtencionGeneradoDTO reporteIncidenteAtendido = historialAtencionesBO.atenderReporteIncidente(reporteAtencion);
         return reporteIncidenteAtendido;
     }
+    
+    /**
+     * Metodo para consultar todos los reportes de atenciones
+     * @return una lista con todos los reportes de atenciones 
+     * @throws NegocioException si ocurre un error al consultar 
+     */
     @Override
     public List<ReporteAtencionDTO> consultarTodosLosReportesAtenciones() throws NegocioException {
         List<ReporteAtencionDTO> reportesAtenciones = historialAtencionesBO.consultarReportesAtenciones();
         return reportesAtenciones;
     }
 
+    /**
+     * Metodo para consultar todos los reportes de atenciones
+     * @return una lista con todos los reportes de incidentes
+     * @throws NegocioException si ocurre un error al consultar  
+     */
     @Override
     public List<ReporteIncidenteDTO> consultarTodosLosReportesIncidentes() throws NegocioException {
         List<ReporteIncidenteDTO> reportesIncidentes = historialIncidentesBO.consultarReportesIncidentes();
         return reportesIncidentes;
     }
     
+    /**
+     * Metodo para consultar un reporte de atencion por su folio
+     * @param folio por el que se consultara el reporte de atencion
+     * @return el reporte de atencion correspondiente a ese folio
+     * @throws NegocioException si ocurre un error al consultar
+     * o si no se encuentra ningun reporte relacionado al folio 
+     */
     @Override
     public ReporteAtencionDTO consultarReporteAtencionPorFolio(String folio) throws NegocioException {
         ReporteAtencionDTO reporteAtencion = historialAtencionesBO.consultarReporteAtencionPorFolio(folio);
         return reporteAtencion;
     }
     
+    /**
+     * Metodo que consulta todos los reportes (Incidentes y Atenciones) para
+     * agruparlos en un solo historial y mostrarlos en vista de administrador
+     * @return una lista con todos los reportes agrupados en uno solo
+     * @throws NegocioException si ocurre un error al consultar o agrupar los 
+     * dos tipos de reportes
+     */
     @Override
     public List<RegistroReporteAdminDTO> consultarTodosLosReportes() throws NegocioException {
         List<RegistroReporteAdminDTO> reportes = historialAtencionesBO.consultarTodosLosRegistrosReportes();
         return reportes;
     }
     
+    /**
+     * Metodo que consulta todos los reportes (Incidentes y Atenciones) con filtros
+     * para agruparlos en un solo historial y mostrarlos en vista de administrador
+     * @param filtros por los que se consultaran todos los reportes 
+     * Por: nombre de cliente, estado del reporte, categoria del reporte, fecha desde, fecha hasta 
+     * @return una lista con los reportes que coincidan con los filtros
+     * @throws NegocioException si ocurre un error al consultar o 
+     * si los filtros no cumplen las validaciones establecidas.
+     */
     @Override
     public List<RegistroReporteAdminDTO> consultarTodosLosReportesFiltrados(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException {
         validarFechasFiltro(filtros.fechaDesde(), filtros.fechaHasta());
@@ -142,6 +235,13 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         return reportes;
     }
     
+    /**
+     * Metodo para agrupar los datos necesarios para generar 
+     * un reporte pdf de los registros solicitados
+     * @param registros a los cuales se les generara el reporte pdf
+     * @return el reporte pdf generado correctamente 
+     * @throws NegocioException si ocurre un error al generar el reporte pdf
+     */
     @Override
     public byte[] generarReportePdf(List<RegistroReporteAdminDTO> registros) throws NegocioException {
         String tituloReporte = TITULO_REPORTE_PDF;
@@ -160,12 +260,30 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
             throw new NegocioException("No se pudo generar el reporte pdf correctamente.",ex);
         }
     }
+    
+    /**
+     * Metodo para iniciar sesion desde la vista de administrador 
+     * @param loginDTO las credenciales del administrador
+     * @return el administrador si pudo iniciar sesion o no
+     * @throws NegocioException si ocurre un error al iniciar sesion
+     * o si las credenciales no cumplen con las validaciones establecidas
+     */
     @Override
     public AdministradorLogueadoDTO iniciarSesion(LoginAdminDTO loginDTO) throws NegocioException {
         validarInicioSesion(loginDTO);
         AdministradorLogueadoDTO administrador = login.iniciarSesion(loginDTO);
         return administrador;
     }
+    
+    /**
+     * Metodo privado que se encarga de validar la 
+     * descripcion de un reporte de incidente
+     * No debe venir vacia, o nula 
+     * Longitud minima 15 caracteres
+     * Longitud maxima 255 caracteres 
+     * @param descripcion que se validara
+     * @throws NegocioException si no cumple con las validaciones establecidas
+     */
     private void validarDescripcionReporteIncidente(String descripcion) throws NegocioException{
         if(descripcion.isEmpty() || descripcion == null){
             throw new NegocioException("La descripcion del reporte de incidente no debe estar vacía.");
@@ -177,6 +295,15 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
             throw new NegocioException("La longitud minima de la descripcion es de 15 caracteres.");
         }
     }
+    
+    /**
+     * Metodo privado para validar la imagen que se insertara en el reporte 
+     * de atencion y/o en el reporte de atencion.
+     * El tamaño maximo de la imagen es 5 Mb
+     * Solo se permite formato PNG o JPG
+     * @param imagen que se validara 
+     * @throws NegocioException si no cumple con las validaciones establecidas
+     */
     private void validarImagen(byte[] imagen) throws NegocioException{
         
         int maxBytes = 5 * 1024 * 1024;
@@ -190,15 +317,24 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
             && (imagen[2] & 0xFF) == 0x4E
             && (imagen[3] & 0xFF) == 0x47;
 
-        boolean esJPG =
-            imagen.length >= 2
-            && (imagen[0] & 0xFF) == 0xFF
-            && (imagen[1] & 0xFF) == 0xD8;
+        boolean esJPG = imagen.length >= 2 && (imagen[0] & 0xFF) == 0xFF && (imagen[1] & 0xFF) == 0xD8;
         
         if(!esPNG && !esJPG){
             throw new NegocioException("Solo se permiten imagenes PNG o JPG.");
         }
     }
+    
+    /**
+     * Metodo privado que valida las fechas que se usan para filtrar.
+     * La fecha desde y fecha hasta son opcional, por lo cual si estas
+     * vienen nulas, simplemente no continuamos con las validaciones.
+     * Fecha desde no puede ser mayor a la fecha hasta
+     * La fecha desde no puede ser mayor a la fecha actual
+     * La fecha hasta no puede ser mayor a la fecha actual
+     * @param fechaDesde fecha que se validara
+     * @param fechaHasta fecha que se validara
+     * @throws NegocioException si no cumple con las validaciones establecidas
+     */
     private void validarFechasFiltro(LocalDate fechaDesde, LocalDate fechaHasta) throws NegocioException{
         if(fechaDesde == null){
             return;
@@ -213,6 +349,15 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
             throw new NegocioException("La fecha desde no puede ser posterior a la fecha hasta.");
         }
     }
+    
+    /**
+     * Metodo privado para validar la solucion de un reporte de atencion
+     * La solucion debe tener minimo 15 caracteres
+     * La solucion debe tener maximo 255 caracteres 
+     * La solucion no dede estar vacia ni nula
+     * @param solucion que se validara
+     * @throws NegocioException si no cumple con las validaciones establecidas
+     */
     private void validarSolucionReporteAtencion(String solucion) throws NegocioException{
         if(solucion.isEmpty() || solucion == null){
             throw new NegocioException("La solucion del reporte de atencion no debe estar vacía.");
@@ -224,22 +369,51 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
             throw new NegocioException("La longitud minima de la solucion es de 15 caracteres.");
         }
     }
+    
+    /**
+     * Metodo privado para validar el nombre del cliente cuando se consulta con filtros
+     * El nombre es opcional, puede venir nulo o estar vacio. Tambien puede ser 
+     * desde una sola letra, pues se puede buscar por coincidencias de letra tambien. No
+     * solo por nombre completo.
+     * El nombre debe tener un maximo de 30 caracteres
+     * @param nombre que se validara 
+     * @throws NegocioException si no cumple con las validaciones establecidas
+     */
     private void validarNombreFiltro(String nombre) throws NegocioException{
         if(nombre.trim().length() > 30){
             throw new NegocioException("El campo para filtrar por nombre debe tener maximo 30 caracteres.");
         }
     }
+    
+    /**
+     * Metodo privado para validar el asunto de un reporte de incidente
+     * El asunto no debe estar vacio, ni nulo
+     * El asunto debe tener minimo 5 caracteres
+     * El asunto debe tener maximo 80 caracteres
+     * @param asunto que se validara
+     * @throws NegocioException si no cumple con las validaciones establecidas
+     */
     private void validarAsuntoReporteIncidente(String asunto) throws NegocioException{
         if(asunto.isEmpty() || asunto == null){
             throw new NegocioException("El asunto del reporte es obligatorio.");
         }
-        if(asunto.trim().length()>100){
+        if(asunto.trim().length()>80){
             throw new NegocioException("El asunto debe contener como maximo 100 caracteres.");
         }
         if(asunto.trim().length() < 5){
             throw new NegocioException("El asunto debe contener como minimo 5 caracteres.");
         }
     }
+    
+    /**
+     * Metodo privado para validar las credenciales con las que se intenta
+     * iniciar sesion como administrador.
+     * Las credenciales no deben venir nulas, ni vacias
+     * La lognitud maxima para la contraseña y para el usuario
+     * es de 30 caracteres
+     * @param login credenciales a validar 
+     * @throws NegocioException 
+     */
     private void validarInicioSesion(LoginAdminDTO login) throws NegocioException{
         if(login == null){
             throw new NegocioException("Los datos de inicio de sesion no pueden ser nulos.");
@@ -247,8 +421,14 @@ public class CUQuejasSugerencias implements ICUQuejasSugerencias{
         if(login.contrasenia().isBlank() || login.contrasenia() == null){
             throw new NegocioException("La contraseña no debe estar vacia.");
         }
+        if(login.contrasenia().length() > 30){
+            throw new NegocioException("La contraseña debe tener maximo 30 caracteres.");
+        }
         if(login.usuario().isBlank() || login.usuario() == null){
             throw new NegocioException("El usuario es obligatorio.");
+        }
+        if(login.usuario().length() > 30){
+            throw new NegocioException("El usuario debe de tener maximo 30 caracteres.");
         }
     }
  
--- CUQuejasSugerencias/src/main/java/com/mycompany/cuquejassugerencias/ICUQuejasSugerencias.java
+++ CUQuejasSugerencias/src/main/java/com/mycompany/cuquejassugerencias/ICUQuejasSugerencias.java
@@ -22,23 +22,148 @@ import java.util.List;
  */
 public interface ICUQuejasSugerencias {
     //Catalogos
+    /**
+     * Metodo que consulta todas las categorias
+     * @return una lista con las categorias 
+     * @throws NegocioException si ocurre un error al consultar
+     */
     public abstract List<CategoriaDTO> cargarCatalogoCategorias() throws NegocioException;
+    
+    /**
+     * Metodo que consulta todos los estados de los reportes
+     * @return una lista con todos los estados
+     * @throws NegocioException si ocurre un error al consultar 
+     */
     public abstract List<EstadoReporteDTO> cargarCatalogoEstados()throws NegocioException;
+    
     //Reportes Incidentes
+    /**
+     * Metodo para consultar todos los reportes de incidentes
+     * utilizando filtros
+     * @param filtros por los que se consultara los reportes
+     * Por: nombre cliente, estado reporte, categoria reporte, fecha desde, fecha hasta
+     * @return una lista con los reportes que coincidan con los filtros o
+     * todos los reportes si los filtros van vacios
+     * @throws NegocioException si ocurre un error al consultar o si los
+     * filtros no cumplen las validaciones establecidas
+     */
     public abstract List<ReporteIncidenteDTO> consultarReportesIncidentes(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException;
+    
+    /**
+     * Metodo para consultar todos los reportes de incidentes generados por un cliente
+     * utilizando filtros
+     * @param filtros por los que se consultara los reportes
+     * Por: estado reporte, categoria reporte, fecha desde, fecha hasta
+     * @return una lista con los reportes del cliente que coincidan con los filtros o
+     * todos los reportes del cliente si los filtros van vacios
+     * @throws NegocioException si ocurre un error al consultar o si los
+     * filtros no cumplen las validaciones establecidas
+     */
     public abstract List<ReporteIncidenteDTO> consultarReportesIncidentesPorCliente(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException;
+    
+    /**
+     * Metodo para generar un reporte de incidente
+     * @param reporteIncidente objeto con los datos necesarios
+     * para generar un nuevo reporte de incidente
+     * @return el reporte de incidente generado correctamente
+     * @throws NegocioException si ocurre un error al generar el reporte
+     */
     public abstract ReporteIncidenteGeneradoDTO generarReporteIncidente(NuevoReporteIncidenteDTO reporteIncidente) throws NegocioException;
+    
+    /**
+     * Metodo para consultar todos los reportes de incidentes
+     * @return una lista con todos los reportes 
+     * @throws NegocioException si ocurre un error al consultar
+     */
     public abstract List<ReporteIncidenteDTO> consultarTodosLosReportesIncidentes() throws NegocioException;
+    
+    /**
+     * Metodo para consultar un reporte de incidente por folio
+     * @param folio por el que se consultara el reporte
+     * @return el reporte de incidente que coincida con el folio
+     * @throws NegocioException si ocurre un error al consultar o 
+     * si no se encuentra ningun reporte relacionado a ese folio
+     */
     public abstract ReporteIncidenteDTO consultarReporteIncidentePorFolio(String folio) throws NegocioException;
+    
     // Reportes Atenciones
+    /**
+     * Metodo para consultar todos los reportes de atenciones
+     * @return una lista con todos los reportes
+     * @throws NegocioException si ocurre un error al consultar
+     */
     public abstract List<ReporteAtencionDTO> consultarTodosLosReportesAtenciones() throws NegocioException;
+    
+    /**
+     * Metodo para consultar todos los reportes de atenciones
+     * utilizando filtros
+     * @param filtros por los que se consultara los reportes
+     * Por: nombre cliente, estado reporte, categoria reporte, fecha desde, fecha hasta
+     * @return una lista con los reportes que coincidan con los filtros o
+     * todos los reportes si los filtros van vacios
+     * @throws NegocioException si ocurre un error al consultar o si los
+     * filtros no cumplen las validaciones establecidas
+     */
     public abstract List<ReporteAtencionDTO> consultarReportesAtenciones(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException;
+    
+    /**
+     * Metodo para atender/resolver un reporte de incidente 
+     * @param reporteAtencion objeto con los datos necesarios
+     * para resolver un reporte de incidente
+     * @return un reporte de atencion generado correctamente
+     * despues de resolver el reporte de incidente
+     * @throws NegocioException si ocurre un error al resolver el reporte
+     * de incidente 
+     */
     public abstract ReporteAtencionGeneradoDTO atenderReporteIncidente(AtenderReporteDTO reporteAtencion) throws NegocioException;
+    
+    /**
+     * Metodo para consultar un reporte de atencion por folio
+     * @param folio por el que se consultara el reporte
+     * @return el reporte de atencion que corresponda al folio
+     * @throws NegocioException si ocurre un error al consultar o
+     * si no se encuentra ningun reporte relacionado a ese folio
+     */
     public abstract ReporteAtencionDTO consultarReporteAtencionPorFolio(String folio) throws NegocioException;
+    
     //Vista admin
+    /**
+     * Metodo para consultar todos los reportes (Atenciones y Incidentes)
+     * y agruparlos juntos para la vista de administrador. 
+     * @return una lista con los dos tipos de reportes agrupados
+     * @throws NegocioException si ocurre un error al agruparlos o al consultarlos
+     */
     public abstract List<RegistroReporteAdminDTO> consultarTodosLosReportes() throws NegocioException;
+    
+    /**
+     * Metodo para consultar todos los reportes por filtro (Atenciones y Incidentes)
+     * y agruparlos juntos para la vista de administrador. 
+     * @param filtros por los que se consultaran todos los reportes
+     * Por: nombre cliente, estado del reporte, categoria del reporte, fecha desde, fecha hasta
+     * @return una lista con los dos tipos de reportes agrupados que coincidan con los filtros
+     * @throws NegocioException si ocurre un error al consultarlos o si 
+     * los filtros no cumplen con las validaciones establecidas
+     */
     public abstract List<RegistroReporteAdminDTO> consultarTodosLosReportesFiltrados(FiltrosConsultaHistorialReportesNegocioDTO filtros) throws NegocioException;
+    
+    /**
+     * Metodo para iniciar sesion a la vista de administrador
+     * @param login las credenciales con las que se intentara
+     * iniciar sesion
+     * @return al administrador si es que pudo iniciar sesion
+     * o nulo si no se pudo iniciar sesion
+     * @throws NegocioException si ocurre un error al iniciar sesion
+     */
     public abstract AdministradorLogueadoDTO iniciarSesion(LoginAdminDTO login) throws NegocioException;
+    
     //Generar pdf
+    /**
+     * Metodo para generar un reporte pdf del historial
+     * del registro de los reportes (Atencion y Incidente)
+     * @param generarReportePdf objeto con los datos necesarios
+     * para generar el reporte pdf
+     * @return el reporte pdf generado correctamente 
+     * @throws NegocioException si ocurre un error al generar el reporte pdf
+     */
     public abstract byte[] generarReportePdf(List<RegistroReporteAdminDTO> generarReportePdf) throws NegocioException;
 }
